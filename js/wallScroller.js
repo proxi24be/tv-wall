972d0e233581e2288e6a9c7806ce508c21ef8575
noobuntu <noobuntu@private.com>
2016-11-12 18:14:00 +0100

    nothing special

--- js/wallScroller.js
+++ js/wallScroller.js
@@ -3,6 +3,8 @@ define(['observableEvent'], function(Observable) {
         var _this = this,
             _scrollPosition = {
                 current : 0,
+                scrollHeight: 0,
+                clientHeight: 0
             },
             _containerId = containerId;
 
@@ -26,6 +28,17 @@ define(['observableEvent'], function(Observable) {
             return container.scrollTop > _scrollPosition.current; 
         }
 
+        function recycleTop(scrollInformation) {
+            var delta = scrollInformation.current - scrollInformation.clientHeight;
+            console.warn('recycleTop', delta);
+        }
+
+        function recycleBottom(scrollInformation) {
+            var delta = 
+                scrollInformation.scrollHeight - scrollInformation.current + scrollInformation.clientHeight;
+            console.warn('recycleBottom', delta);
+        }
+
         function initListener() {
             getContainer().addEventListener('scroll', function(event) {
                 if (scrollUp(this)) {
@@ -41,6 +54,8 @@ define(['observableEvent'], function(Observable) {
                 }
 
                 _scrollPosition.current = this.scrollTop;
+                _scrollPosition.scrollHeight = this.scrollHeight;
+                _scrollPosition.clientHeight = this.clientHeight;
             });
         }
 
@@ -56,6 +71,14 @@ define(['observableEvent'], function(Observable) {
         _this.EVENT_REACHED_TOP = 'WallScroller.reachedTop';
         _this.EVENT_SCROLL_UP = 'WallScroller.scrollUp';
         _this.EVENT_SCROLL_DOWN = 'WallScroller.scrollDown';
+
+        _this.getScrollInformation = function() {
+            return _scrollPosition;
+        }
+
+        _this.recycleTop = recycleTop;
+
+        _this.recycleBottom = recycleBottom;
     };
 
     return WallScroller;
